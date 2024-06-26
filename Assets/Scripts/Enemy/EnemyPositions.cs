d77646964c33cb3737958edd4f8c6be1202d3833
Evgenii <justmysimplemail@gmail.com>
2024-06-26 14:50:21 +0500

    Made method static for EnemyPositions

--- Assets/Scripts/Enemy/EnemyPositions.cs
+++ Assets/Scripts/Enemy/EnemyPositions.cs
@@ -5,20 +5,19 @@ namespace ShootEmUp
     public class EnemyPositions : MonoBehaviour
     {
         [SerializeField] private Transform[] spawnPositions;
-
         [SerializeField] private Transform[] attackPositions;
 
         public Transform RandomSpawnPosition()
         {
-            return this.RandomTransform(this.spawnPositions);
+            return RandomTransform(spawnPositions);
         }
 
         public Transform RandomAttackPosition()
         {
-            return this.RandomTransform(this.attackPositions);
+            return RandomTransform(attackPositions);
         }
 
-        private Transform RandomTransform(Transform[] transforms)
+        private static Transform RandomTransform(Transform[] transforms)
         {
             var index = Random.Range(0, transforms.Length);
             return transforms[index];
