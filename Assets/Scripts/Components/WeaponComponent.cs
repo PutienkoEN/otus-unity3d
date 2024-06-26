faee8ef2b0ae9a409fa9a19cd19498c0f0b78158
Evgenii <justmysimplemail@gmail.com>
2024-06-26 14:43:21 +0500

    Refactor weapon component

--- Assets/Scripts/Components/WeaponComponent.cs
+++ Assets/Scripts/Components/WeaponComponent.cs
@@ -8,7 +8,6 @@ namespace ShootEmUp
     {
         [SerializeField] private BulletSpawner bulletSpawner;
         [SerializeField] private BulletConfig bulletConfig;
-
         [SerializeField] private Transform firePoint;
 
         public void Initialize(BulletSpawner bulletSpawner)
@@ -18,17 +17,32 @@ namespace ShootEmUp
 
         public void Attack(Transform target)
         {
-            var position = new Vector2(firePoint.position.x, firePoint.position.y);
-            var direction = (new Vector2(target.position.x, target.position.y) - position).normalized;
+            var firePosition = GetFirePosition();
+            var direction = GetTargetPosition(target, firePosition);
 
             var bulletData = new BulletSpawner.BulletData
             {
                 BulletConfig = bulletConfig,
-                Position = position,
+                Position = firePosition,
                 Velocity = direction * bulletConfig.speed
             };
 
             bulletSpawner.SpawnBullet(bulletData);
+        }
+
+        private Vector2 GetFirePosition()
+        {
+            var firePointPosition = firePoint!.position;
+            return new Vector2(firePointPosition.x, firePointPosition.y);
+        }
+
+        private static Vector2 GetTargetPosition(Transform target, Vector2 firePosition)
+        {
+            var position = target!.position;
+            var targetPosition = new Vector2(position.x, position.y);
+
+            var direction = targetPosition - firePosition;
+            return direction.normalized;
         }
     }
 }
