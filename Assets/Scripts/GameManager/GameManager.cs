ee541ee12a8b945a7ea961a9ccc820ec10332728
Evgenii <justmysimplemail@gmail.com>
2024-06-28 04:54:09 +0500

    Add states to the game.

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ShootEmUp
@@ -6,41 +7,99 @@ namespace ShootEmUp
     {
         [SerializeField] private GameStartTimer gameStartTimer;
 
+        private GameState currentState = GameState.None;
         private GameListenerManager gameListenerManager = new();
+
+        private State StartGameState = new(new HashSet<GameState> { GameState.None });
+        private State PauseGameState = new(new HashSet<GameState> { GameState.InProgress });
+        private State ResumeGameState = new(new HashSet<GameState> { GameState.Paused });
+        private State FinishGameState = new(new HashSet<GameState> { GameState.InProgress });
 
         public void StartGame()
         {
+            if (StartGameState.IsNotAllowedTransition(currentState))
+            {
+                return;
+            }
+
+            currentState = GameState.AwaitStart;
             gameStartTimer.StartTimer(GameStartedAction);
         }
 
         private void GameStartedAction()
         {
             gameListenerManager.TriggerGameStartListeners();
+            currentState = GameState.InProgress;
+
             Debug.Log("Game started!");
         }
 
         public void FinishGame()
         {
+            if (FinishGameState.IsNotAllowedTransition(currentState))
+            {
+                return;
+            }
+
             gameListenerManager.TriggerGameFinishListeners();
+            currentState = GameState.Finished;
+
             Debug.Log("Game finished!");
         }
 
         public void PauseGame()
         {
+            if (PauseGameState.IsNotAllowedTransition(currentState))
+            {
+                return;
+            }
+
             gameListenerManager.TriggerGamePauseListeners();
+            currentState = GameState.Paused;
+
             Debug.Log("Game paused!");
         }
 
         public void ResumeGame()
         {
+            if (ResumeGameState.IsNotAllowedTransition(currentState))
+            {
+                return;
+            }
+
             gameListenerManager.TriggerGameResumeListeners();
+            currentState = GameState.InProgress;
+
             Debug.Log("Game resumed!");
         }
     }
 
-    internal enum GameState
+    public enum GameState
     {
+        None,
+        AwaitStart,
         InProgress,
+        Paused,
         Finished
+    }
+
+    public class State
+    {
+        private readonly HashSet<GameState> allowedStates;
+
+        public State(HashSet<GameState> allowedStates)
+        {
+            this.allowedStates = allowedStates;
+        }
+
+        public bool IsAllowedTransition(GameState gameState)
+        {
+            return allowedStates.Contains(gameState);
+        }
+
+        public bool IsNotAllowedTransition(GameState gameState)
+        {
+            return !IsAllowedTransition(gameState);
+        }
     }
 }
